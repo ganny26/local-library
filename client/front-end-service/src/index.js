01e2e40bdf9e59beac77c642807cb5385c339ed0
shriram <shriram.balakrishnan@in.reportbee.com>
2019-05-14 21:52:08 +0530

    Use react-router for front-end routing

--- client/front-end-service/src/index.js
+++ client/front-end-service/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./index.css";
 
 class Book extends React.Component {
@@ -69,4 +70,48 @@ class BookList extends React.Component {
   }
 }
 
-ReactDOM.render(<BookList />, document.getElementById("root"));
+function Home() {
+  return <div>Home</div>;
+}
+
+function About() {
+  return <div>About Us</div>;
+}
+
+class BookCreator extends React.Component {
+  render() {
+    return <div>Form to create book appears here!!!</div>;
+  }
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/books">Books List</Link>
+            </li>
+            <li>
+              <Link to="/create_book">Create Book</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/books" component={BookList} />
+        <Route path="/create_book" component={BookCreator} />
+      </div>
+    </Router>
+  );
+}
+
+ReactDOM.render(<AppRouter />, document.getElementById("root"));
