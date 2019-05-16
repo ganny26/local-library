bcb444ba6bbed7187db9b9d9c38fef006e48c20b
Selvaganesh <selvaganeshrajam@gmail.com>
2019-05-16 19:33:45 +0530

    Webpack implemented

--- client/front-end-service/webpack.config.js
+++ client/front-end-service/webpack.config.js
@@ -1,25 +1,42 @@
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+console.log("path...", path.join(__dirname, "/src/index.js"));
 module.exports = {
+  mode: "development",
   entry: path.join(__dirname, "/src/index.js"),
   output: {
     filename: "build.js",
-    path: path.join(__dirname, "/dist")
+    path: path.join(__dirname, "/dist"),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+    hot: true,
+    inline: true,
+    open: true,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
   },
   module: {
     rules: [
       {
-        test: /\.js$ /,
+        test: /\.js$/,
         exclude: / node_modules /,
-        loader: "babel-loader"
-      }
-    ]
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.join(__dirname, "src", "index.html")
-    })
-  ]
+      filename: "./index.html",
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
 };
